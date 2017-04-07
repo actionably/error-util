927e500cc9ca2fab008ba1711d2a4ee61f2ec1ec
Jesse Hull <jessehull@gmail.com>
2017-04-06 20:54:06 -0700

    added airbrake and lambda handler

--- error-util.js
+++ error-util.js
@@ -1,8 +1,48 @@
 'use strict'
 
 const _ = require('lodash')
+const q = require('q')
 
 class ErrorUtil {
+  initializeAirbrake() {
+    if (!this.airbrake) {
+      if (process.env.AIRBRAKE_PROJECT_ID && process.env.AIRBRAKE_PROJECT_API_KEY) {
+        this.airbrake = require('airbrake').createClient(
+          process.env.AIRBRAKE_PROJECT_ID,
+          process.env.AIRBRAKE_PROJECT_API_KEY
+        )
+        this.airbrake.handleExceptions()
+      }
+    }
+  }
+
+  handleLambda(promise, event, context, callback)  {
+    q(promise).done(retValue => {
+      callback(null, retValue)
+    }, error => {
+      this.initializeAirbrake()
+      console.error('ERROR500:: ', JSON.stringify(event))
+      console.error('ERROR500:: ', JSON.stringify(context))
+      console.error('ERROR500:: ', JSON.stringify(error))
+      console.error('ERROR500:: ', error.stack)
+      error.message += `event = ${JSON.stringify(event)} 
+           context = ${JSON.stringify(context)}
+          ${this.errorToString(error)}`
+      if (this.airbrake) {
+        this.airbrake.notify(error, (err2, url) => {
+          if (err2) {
+            console.error('unable to save to airbrake ' + this.errorToString(err2))
+          }
+          console.error('Error saved to airbrake ' + url)
+        })
+      }
+      const errorReturn = {
+        errors: [{message: JSON.stringify(error) + ' ' + error.stack}]
+      }
+      callback(JSON.stringify(errorReturn))
+    })
+  }
+
   errorToString(err) {
     let s = ''
     const stringify = JSON.stringify(err)
